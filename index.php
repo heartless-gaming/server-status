e71a3d77142168b2792449b5cb8541ccade6389c
skullmasher <skullmasher@heartlessgaming.com>
2016-08-06 12:02:59 +0200

    adding Heartless gaming server map variable

--- index.php
+++ index.php
@@ -8,6 +8,48 @@ require __DIR__ . '/vendor/koraktor/steam-condenser/lib/steam-condenser.php';
 
 $hls_server_ip = '91.121.154.84';
 
+$hls_server_map = [ 
+    'csgo' => [
+        [
+            'title' => 'Heartless Gaming | Public Competitive - tick 128',
+            'port' => 27015
+        ],[
+            'title' => 'Heartless Gaming | Public Competitive - tick 128',
+            'port' => 27016  
+        ]
+    ],
+    'insurgency' => [
+        [
+            'title' => 'Heartless Gaming | Insurgency COOP - tick 128',
+            'port' => 27018
+        ]
+    ],
+    'hl2dm' => [
+        [
+            'title' => 'Heartless Gaming | Half-Life 2 Deathmatch - Vanilla',
+            'port' => 27021
+        ]
+    ],
+    'bms' => [
+        [
+            'title' => 'Heartless Gaming | Black Mesa Source - Vanilla',
+            'port' => 27022
+        ]
+    ],
+    'killingfloor' => [
+        [
+            'title' => 'Heartless Gaming | HARD Long',
+            'port' => 7707
+        ],[
+            'title' => 'Heartless Gaming | HARD Suicidal',
+            'port' => 7708
+        ],[
+            'title' => 'Heartless Gaming | Killing Floor Private',
+            'port' => 7709
+        ]
+    ]
+];
+
 // You can either read the steamapi result from a URL or from a file.
 $hls_steamapi_url = 'http://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=' . $hls_server_ip . '&format=json';
 $hls_steamapi_file = 'heartlessgaming-steamapi.json';
@@ -28,3 +70,4 @@ echo '</pre>';
  *
  *
  */
+
