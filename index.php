237a36cb49704b883c45d9db06b7e22ac1200904
skullmasher <skullmasher@heartlessgaming.com>
2016-08-06 03:26:03 +0200

    Rsearch about killing floor server query

--- index.php
+++ index.php
@@ -8,7 +8,23 @@ require __DIR__ . '/vendor/koraktor/steam-condenser/lib/steam-condenser.php';
 
 $hls_server_ip = '91.121.154.84';
 
-// json from Valve Master server
+// You can either read the steamapi result from a URL or from a file.
 $hls_steamapi_url = 'http://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=' . $hls_server_ip . '&format=json';
+$hls_steamapi_file = 'heartlessgaming-steamapi.json';
+
+// Getting an array of the online servers from steamapi
 $hls_steamapi_json = json_decode(file_get_contents($hls_steamapi_url), true);
 $hls_online_servers = $hls_steamapi_json['response']['servers'];
+
+echo '<a href="' . $hls_steamapi_url . '">'. $hls_steamapi_url . '</a>';
+echo '<pre>';
+var_dump($hls_online_servers);
+echo '</pre>';
+
+/*
+ * Get server information using their query protocol
+ * 
+ * NODEJS : https://github.com/kurt-stolle/game-server-query
+ *
+ *
+ */
