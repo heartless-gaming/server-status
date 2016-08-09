07c85e04fa083bd6d9f33824d94d66ed4765a18c
skullmasher <skullmasher@heartlessgaming.com>
2016-08-09 12:30:42 +0200

    Adding editorconfig & Header is done

--- app/index.php
+++ app/index.php
@@ -10,45 +10,45 @@ $hls_server_ip = '91.121.154.84';
  * TODO : Grab this from a json file
  */
 $hls_server_map = [
-    'Counter-Strike : Global Offensive' => [
-        [
-            'servername' => 'Heartless Gaming | Public Competitive - tick 128',
-            'port' => 27015
-        ],[
-            'servername' => 'Heartless Gaming | Deathmatch FFA Only D2 - tick 128',
-            'port' => 27016  
-        ]
-    ],
-    'Killing Floor' => [
-        [
-            'servername' => 'Heartless Gaming | HARD Long',
-            'port' => 7707
-        ],[
-            'servername' => 'Heartless Gaming | HARD Suicidal',
-            'port' => 7708
-        ],[
-            'servername' => 'Heartless Gaming | Killing Floor Private',
-            'port' => 7709
-        ]
-    ],
-    'Insurgency' => [
-        [
-            'servername' => 'Heartless Gaming | Insurgency COOP - tick 128',
-            'port' => 27018
-        ]
-    ],
-    'Half-life 2: Deathmatch' => [
-        [
-            'servername' => 'Heartless Gaming | Half-Life 2 Deathmatch - Vanilla',
-            'port' => 27021
-        ]
-    ],
-    'Black Mesa Source Multiplayer' => [
-        [
-            'servername' => 'Heartless Gaming | Black Mesa Source - Vanilla',
-            'port' => 27022
-        ]
-    ]
+	'Counter-Strike : Global Offensive' => [
+		[
+			'servername' => 'Heartless Gaming | Public Competitive - tick 128',
+			'port' => 27015
+		],[
+			'servername' => 'Heartless Gaming | Deathmatch FFA Only D2 - tick 128',
+			'port' => 27016
+		]
+	],
+	'Killing Floor' => [
+		[
+			'servername' => 'Heartless Gaming | HARD Long',
+			'port' => 7707
+		],[
+			'servername' => 'Heartless Gaming | HARD Suicidal',
+			'port' => 7708
+		],[
+			'servername' => 'Heartless Gaming | Killing Floor Private',
+			'port' => 7709
+		]
+	],
+	'Insurgency' => [
+		[
+			'servername' => 'Heartless Gaming | Insurgency COOP - tick 128',
+			'port' => 27018
+		]
+	],
+	'Half-life 2: Deathmatch' => [
+		[
+			'servername' => 'Heartless Gaming | Half-Life 2 Deathmatch - Vanilla',
+			'port' => 27021
+		]
+	],
+	'Black Mesa Source Multiplayer' => [
+		[
+			'servername' => 'Heartless Gaming | Black Mesa Source - Vanilla',
+			'port' => 27022
+		]
+	]
 ];
 
 /*
@@ -67,16 +67,16 @@ $hls_online_servers_port = [];
 
 // Populating $hls_online_servers_port array to use for server online status detection
 foreach ($hls_online_servers as $hls_online_server) {
-    array_push($hls_online_servers_port, $hls_online_server['gameport']);
+	array_push($hls_online_servers_port, $hls_online_server['gameport']);
 }
 
 // Checking online status against the steamapi result. Also check for type int.
 function check_server_status($gameport) {
-    global $hls_online_servers_port;
+	global $hls_online_servers_port;
 
-    if (in_array($gameport, $hls_online_servers_port, TRUE)) {
-        return true;
-    }
+	if (in_array($gameport, $hls_online_servers_port, TRUE)) {
+		return true;
+	}
 }
 
 /*
@@ -85,50 +85,48 @@ function check_server_status($gameport) {
  * NOT USED just there for later use
  */
 function get_source_server_info($port) {
-    global $hls_server_ip;
+	global $hls_server_ip;
 
-    $server = new SourceServer( $hls_server_ip, $port );
-    $server->initialize();
-    return $server->getServerInfo();
+	$server = new SourceServer( $hls_server_ip, $port );
+	$server->initialize();
+	return $server->getServerInfo();
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Game server status of the Heartless Gaming pc gaming community.">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Game Server Status | Heartless Gaming</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Game server status of the Heartless Gaming pc gaming community.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Game Server Status | Heartless Gaming</title>
 </head>
 <body>
-    <header class="page-head">
-        <div class="flex-container">
-            <h1 class="page-head__text">Game Server Status</h1>
-            <img src="img/hearltess-gaming-logo.svg" alt="Logo Heartless Gaming" class="page-head__logo">
-        </div>
-    </header>
-    <main>
-        <?php foreach ($hls_server_map as $gamename => $hls_gameservers_info) : ?>
-            <section>
-                <h2><?php echo $gamename ?></h2>
-                <?php foreach ($hls_gameservers_info as $game_info) : ?>
-                    <p><?php echo $game_info['servername'] ?></p>
-                    <p><?php echo $hls_server_ip . ':' . $game_info['port'] ?></p>
-                    <?php if (check_server_status($game_info['port'])) : ?>
-                        <a href="steam://connect/<?php echo $hls_server_ip . ':' . $game_info['port'] ?>"><button>Join</button></a>
-                    <?php else: ?>
-                        <p>Server is offline</p>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </section>
-        <?php endforeach; ?>
-    </main>
-    <footer>
-        <p>Feel free to <a href="mailto:contact@heartlessgaming.com">contact us</a> if you have a problem or a sugestion to make the game servers better.</p>
-        <p>The source code of this website is available on <a href="https://github.com/heartless-gaming/server-status">github</a></p>
-        <p>Play more, Care less, Be an Heartless.</p>
-    </footer>
+	<header class="page-header flex-container">
+			<h1 class="page-header__title">Game Server Status</h1>
+			<img src="img/hearltess-gaming-logo.svg" alt="Logo Heartless Gaming" class="page-header__logo">
+	</header>
+	<main>
+		<?php foreach ($hls_server_map as $gamename => $hls_gameservers_info) : ?>
+			<section>
+				<h2><?php echo $gamename ?></h2>
+				<?php foreach ($hls_gameservers_info as $game_info) : ?>
+					<p><?php echo $game_info['servername'] ?></p>
+					<p><?php echo $hls_server_ip . ':' . $game_info['port'] ?></p>
+					<?php if (check_server_status($game_info['port'])) : ?>
+						<a href="steam://connect/<?php echo $hls_server_ip . ':' . $game_info['port'] ?>"><button>Join</button></a>
+					<?php else: ?>
+						<p>Server is offline</p>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			</section>
+		<?php endforeach; ?>
+	</main>
+	<footer>
+		<p>Feel free to <a href="mailto:contact@heartlessgaming.com">contact us</a> if you have a problem or a sugestion to make the game servers better.</p>
+		<p>The source code of this website is available on <a href="https://github.com/heartless-gaming/server-status">github</a></p>
+		<p>Play more, Care less, Be an Heartless.</p>
+	</footer>
 </body>
 </html>
